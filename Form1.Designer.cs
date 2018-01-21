898e578b52cba41fbdd8892a7a4f0afcef3e28cb
Shtanga13 <31135498+Shtanga13@users.noreply.github.com>
2018-01-21 17:33:14 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PrimeChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 237);
+            this.panel1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(15, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 25);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Show factors";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(15, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(209, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(162, 76);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(209, 112);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(162, 137);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AccessibleDescription = "A program that checks whether a number is prime.";
+            this.AccessibleName = "Prime Checker";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(383, 261);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prime Checker";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeChecker
+{
+
+    public partial class Form1 : Form
+    {
+        private string tb2, tb3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CleanTextFields()
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CleanTextFields();
+            if (long.TryParse(textBox1.Text, out long a))
+            {
+                Number n = new Number
+                {
+                    value = long.Parse(textBox1.Text)
+                };
+                n.prime = n.CheckPrime(N: n.value);
+                if(n.prime)
+                {
+                    textBox3.Visible = false;
+                    tb2 = String.Format("The number {0} is prime.", n.value);
+                    textBox2.Text = tb2;
+                }
+                else
+                { 
+                    if(checkBox1.Checked)
+                    {
+                        textBox3.Visible = true;
+                        List<long> fac = new List<long>();
+                        for (long h = 2; h < n.value; h++)
+                        {
+                            if (n.value % h == 0)
+                            {
+                                fac.Add(h);
+                            }
+                        }
+                        n.factorsList = fac.ToArray();
+                        textBox2.Text = String.Format("The number {0} is not prime. ", n.value);
+                        tb3 = "Factors: ";
+                        for(int i = 0; i < n.factorsList.Length; i++)
+                        {
+                            tb3 += n.factorsList[i];
+                            if (i != n.factorsList.Length - 1)
+                            {
+                                tb3 += ", ";
+                            }
+                            else
+                            {
+                                tb3 += ".";
+                            }
+                        }
+                        textBox3.Text = tb3;
+                    }
+                    else
+                    {
+                        textBox3.Visible = false;
+                        tb2 = String.Format("The number {0} is not prime.", n.value);
+                        textBox2.Text = tb2;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a whole number!","Wrong input!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    public class Number
+    {
+        public long value;
+        public int numberFactors;
+        public bool prime;
+        public long[] factorsList;
+        public bool CheckPrime(long N)
+        {
+            if (N == 0)
+            {
+                this.prime = false;
+            }
+            if (N == 1)
+            {
+                this.prime = false;
+            }
+
+            if (N == 2)
+            {
+                this.prime = true;
+            }
+            if (N == 3)
+            {
+                this.prime = true;
+            }
+            this.prime = true;
+            for (int a = 2; a <= Math.Sqrt(N); a++)
+            {
+                if (N % a == 0)
+                {
+                    this.prime = false;
+                    break;
+                }
+            }
+            return this.prime;
+        }
+    }
+}
